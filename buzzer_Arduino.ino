811e60bd69ac50007f8ca2289c3246de6014651e
bozicsasa <47472562+bozicsasa@users.noreply.github.com>
2019-03-16 09:14:30 +0100

    Part 1

--- buzzer_Arduino.ino
+++ buzzer_Arduino.ino
@@ -1,17 +1,48 @@
-
-int sound = 5;
-
+//Bozic & Bragagnolo
+int ledGreen;
+int ledRed;
+int ledPause;
+int buzzer;
+int button;
+int buttonHIGH_or_LOW
+int risultatoRiflessiLed
+int risultatoRiflessiBuzzer;
+ 
 
 void setup() {
-  pinMode(led, OUTPUT);
-  pinMode(sound, OUTPUT);
-}
+  ledGreen = 6;
+  ledRed = 7;
+  ledPause = 8;
+  buzzer = 9;
+  button  10;
+  pinMode(ledGreen, OUTPUT);
+  pinMode(ledRed, OUTPUT);
+  pinMode(ledPause, OUTPUT);
+  pinMode(buzzer, OUTPUT);
+  pinMode(button, INPUT);
+  Serial.begin(9600);
+  
 
+}
+int TryReflex(String testo, int ledBuzzer)
+{
+  delay(random(1000,5000));
+  digitalWrite(ledBuzzer, HIGH);
+  int InizioProva = millis();
+  buttonHIGH_o_LOW = digitalRead(button);
+  bool premuto = false;
+  while(!premuto) 
+  {
+   if(buttonHIGH_or_LOW == HIGH)
+   {
+    premuto = true;
+   }
+  }
+  digitalWrite(ledBuzzer, LOW);
+  int TempoFineProva = mills();
+  int TempoFinale =
+  
 void loop() {
-  digitalWrite(led, HIGH);
-  digitalWrite(sound, HIGH);
-  delay(1000);
-  digitalWrite(led, LOW);
-  digitalWrite(sound, LOW);
-  delay(1000);
+  
+
 }
